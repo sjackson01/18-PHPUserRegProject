c44b3f556b2f44b72929e48b03697a27a02f1efc
sjackson01 <stevenpjackson0@gmail.com>
2020-03-12 09:53:57 -0500

    Fix (footer.php>Hunk 1) : Improve formatting. Fix (header.php>Hunk 1) : Improve formatting.

--- inc/footer.php
+++ inc/footer.php
@@ -5,35 +5,31 @@
     <a href="index.php" title="Home Page">Home</a><br>
     <?php # Script 18.2 - footer.php
     // This page completes the HTML template
-    
+
     // Display links based upon the login status
-    if (isset($_SESSION['user_id'])){
+    if (isset($_SESSION['user_id'])) {
 
         echo '<a href="logout.php" title="Logout">Logout</a><br> 
         <a href="change_password.php" title="Change Your Password">Change Password</a><br>';
-    
 
         // Add links if the user is an administrator:
-        if ($_SESSION['user_level'] == 1){
+        if ($_SESSION['user_level'] == 1) {
             echo '<a href="view_users.php" title="View All Users">View Users</a><br> 
                   <a href="#">Some Admin Page</a><br>';
         }
-
     } else { // Not logged in  
 
         echo '<a href="register.php" title="Register for the Site">Register</a><br>
               <a href="login.php" title="Login">Login</a><br>
               <a href="forgot_password.php" title="Password Retrieval">Retrieve Password</a><br>';
-
     }
     ?>
     <a href="#">Some Page</a><br>
     <a href="#">Another Page</a><br>
-  </div><!-- Menu -->
-  </body>
-  </html>
-  <?php // Fulsh the buffered output.
-  ob_end_flush();  
-  ?>
-    
-    
+</div><!-- Menu -->
+</body>
+
+</html>
+<?php // Fulsh the buffered output.
+ob_end_flush();
+?>
