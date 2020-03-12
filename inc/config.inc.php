170a0c8438e8711a58b5730f0be21bc5b7b8fef0
sjackson01 <stevenpjackson0@gmail.com>
2020-03-12 12:48:17 -0500

    Add (config.inc.php>Hunk 1) : Create config file.

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -23,7 +23,47 @@ define('BASE_URL', 'http://www.example.com');
 define('MYSQL', '/path/to/mysqli_connect');
 
 // Adjust the time zone for PHP 5.1 and greater: 
-date_default_timezone_set('American/Chicago');  
+date_default_timezone_set('American/Chicago');
 
 // ************ SETTINGS ************ //
 // ********************************** //
+
+// ****************************************** //
+// ************ ERROR MANAGEMENT ************ //
+
+// Creat the rror handler:
+function my_error_handler($e_number, $e_message, $e_file, $e_line, $e_vars)
+{
+
+  // Build the error message:
+  $message = "An error occurred in script '$e_file' on line $e_line: $e_message\n";
+
+  // Add the date and time: 
+  $message .= "Date/Time: " . date('n-j-Y H:i:s') . "\n";
+
+  if (!LIVE) { // Development (print the error). 
+
+    // Show the error message: 
+    echo '<div class="error"> . n12br($message)';
+
+    // Add the variables and a backtrace:
+    echo '<pre>' . print_r($e_vars, 1) . "\n";
+    debug_print_backtrace();
+    echo '</pre></div>';
+  } else { // Don't show the error 
+
+    // Send email to the admin:
+    $body = $message . "\n" . print_r($e_vars, 1);
+    mail(EMAIL, 'Site Error', $body, 'From: email@example.com');
+
+    // Only print an error message if the error isn't a notice:
+    if ($e_number != E_NOTICE) {
+      echo '<div class="error">A system error occurred. We apologize for the
+      inconvenience.</div><br>';
+    }
+  } // End of !LIVE IF. 
+
+} // End of my_error_handler() defintion. 
+
+// Use my error handler: 
+set_error_handler('my_error_handler');
